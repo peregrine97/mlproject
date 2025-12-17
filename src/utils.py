45e0b8766891c0e1149cec844b25018519d39be2
peregrine97 <amarssrh@gmail.com>
2025-12-17 20:38:35 +0530

    Data Transformation is added

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import dill 
+import os,sys
+from src.exception import customexception
+
+def save_object(file_path,obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)
+
+    except Exception as e:
+        raise customexception(e,sys)
